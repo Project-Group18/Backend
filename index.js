943a750a10e9929afd42df2940bb4a747a1b9206
SamuliM <t9musa00@students.oamk.ficode .git config --global user.email t9musa00@students.oamk.fi>
2021-11-08 09:45:32 +0200

    API connected to MySQL databaase

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const mysql = require('mysql')
+
+//connection details
+const connection = mysql.createConnection({
+  host:'eu-cdbr-west-01.cleardb.com',
+  user:'b002b54b181543',
+  password:'3da37abf',
+  database:'heroku_f525f990b893a93'
+})
+
+connection.query('SELECT * FROM customer WHERE customer_id="5"', (error, data) => {
+
+  if (error) throw error;
+
+  if(!error) {
+    console.log(data);
+  }
+
+})
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
