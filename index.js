9c8d42fa89cada169ce6b8e5077660c83ba0d860
SamuliM <t9musa00@students.oamk.ficode .git config --global user.email t9musa00@students.oamk.fi>
2021-11-09 08:53:20 +0200

    DB relationships added + connection details updated

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const app = express()
 const port = process.env.PORT || 3000
 const mysql = require('mysql')
 
-//connection details
-const connection = mysql.createConnection({
+//connection details delivertwist18
+/* const connection = mysql.createConnection({
   host:'eu-cdbr-west-01.cleardb.com',
   user:'b002b54b181543',
   password:'3da37abf',
   database:'heroku_f525f990b893a93'
+}) */
+
+
+//connection details delivertwistt18
+const connection = mysql.createConnection({
+  host:'eu-cdbr-west-01.cleardb.com',
+  user:'b02b9eff68ed9d',
+  password:'ecfca0f4',
+  database:'heroku_8de5a5a6f07908e'
 })
 
 connection.query('SELECT * FROM customer WHERE customer_id="5"', (error, data) => {
