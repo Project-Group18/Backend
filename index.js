70f95922a40e43a77c1b2b3977d7d609138c71f6
SamuliM <t9musa00@students.oamk.ficode .git config --global user.email t9musa00@students.oamk.fi>
2021-11-11 10:23:00 +0200

    first commit

--- index.js
+++ index.js
@@ -4,37 +4,22 @@ const port = process.env.PORT || 3000
 const mysql = require('mysql')
 
 //connection details delivertwist18
-/* const connection = mysql.createConnection({
+ const connection = mysql.createConnection({
   host:'eu-cdbr-west-01.cleardb.com',
   user:'b002b54b181543',
   password:'3da37abf',
   database:'heroku_f525f990b893a93'
-}) */
-
-
-//connection details delivertwistt18
-const connection = mysql.createConnection({
-  host:'eu-cdbr-west-01.cleardb.com',
-  user:'b02b9eff68ed9d',
-  password:'ecfca0f4',
-  database:'heroku_8de5a5a6f07908e'
-})
-
-connection.query('SELECT * FROM customer WHERE customer_id="5"', (error, data) => {
-
-  if (error) throw error;
-
-  if(!error) {
-    console.log(data);
-  }
-
-})
-
-
+}) 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  connection.query("SELECT * from customer", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
