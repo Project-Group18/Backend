ff15ea3d5fc6b0ed2ce2f358f7314074911696ac
SamuliM <t9musa00@students.oamk.ficode .git config --global user.email t9musa00@students.oamk.fi>
2021-11-13 21:59:09 +0200

    added cors and port 3000->3001

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const db = require('./database');
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 //for testing purposes
 app.get('/', (req, res) => {
