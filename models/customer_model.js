6923ee6ac6a55f56653766ff77e73023269b3cae
SamuliM <t9musa00@students.oamk.ficode .git config --global user.email t9musa00@students.oamk.fi>
2021-11-15 10:05:57 +0200

    customer post changed to include more fields

--- models/customer_model.js
+++ models/customer_model.js
@@ -7,8 +7,8 @@ const customer = {
   },
   add: function(customer, callback) {
     return connection.query(
-      'insert into customer (customer_name) values (?)', 
-      [customer.customer_name], callback
+      'insert into customer (customer_name, customer_email, home_address, credit_card, customer_password) values (?,?,?,?,?)', 
+      [customer.customer_name, customer.customer_email, customer.home_address, customer.credit_card, customer.customer_password], callback
       );
   }
 };
