ef741f415ea87e1cab6b13e5e1ebe7264db44034
NLAIV <juho.nuorala@gmail.com>
2021-11-26 09:19:36 +0200

    Solved Dish-table
    Solved Dish table not updating on request, added missing data.

--- models/dish_model.js
+++ models/dish_model.js
@@ -9,8 +9,8 @@ const dish = {
     return connection.query('select * from dish where dish_id=?', [id], callback);
   },
   add: function(dish, callback) {
-    return connection.query('insert into dish (dish_name, price, category_id, dish_picture, dish_info) values (?,?,?,?,?)', 
-    [dish.dish_name, dish.price, dish.category_id, dish.dish_picture, dish.dish_info], callback
+    return connection.query('insert into dish (dish_name, price, category_id, dish_picture, dish_info, restaurant_id) values (?,?,?,?,?,?)', 
+    [dish.dish_name, dish.price, dish.category_id, dish.dish_picture, dish.dish_info, dish.restaurant_id], callback
     );
   }
 };
