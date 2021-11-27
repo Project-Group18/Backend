27389d6ce56fec9fde7afd3aa908f632fb2372c6
SamuliM <t9musa00@students.oamk.ficode .git config --global user.email t9musa00@students.oamk.fi>
2021-11-27 13:56:03 +0200

    added routes to all tables

--- routes/dish.js
+++ routes/dish.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const dish = require('../models/dish_model');
 
-
-  //
+  
   //get dish by restaurant id
 router.get('/:id?',
  function(req, res) {
